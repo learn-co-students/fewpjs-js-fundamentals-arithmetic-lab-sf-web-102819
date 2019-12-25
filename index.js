9b7b3d31b91d00185b6861cf737014e89dbdae1e
Allen Shin <geeuho@gmail.com>
2019-12-25 01:03:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code here\
+let num1 = 2
+let num2 = 31
+
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = 19 % 5
+
+const max = Math.max(20,3,5)
